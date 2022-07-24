cea47982e2569923d81c96ccb83ab9d819081fc3
Jure Bogunovic <bogunovij@gmail.com>
2022-07-24 18:41:34 +0200

    dependencies now use the new wrapper page

--- src/components/WrapperDependencyView.tsx
+++ src/components/WrapperDependencyView.tsx
@@ -1,59 +1,42 @@
 import { WrapperInfo } from "../models/WrapperInfo";
 
-import { useEthers } from "@usedapp/core";
+import Link from "next/link";
+
+const getDependencyUrl = (wrapUri: string): string => {
+  if (wrapUri.startsWith("ens/")) {
+    const domainWithNetwork = wrapUri.slice("ens/".length, wrapUri.length);
+    if (domainWithNetwork.includes("/")) {
+      const network = domainWithNetwork.split("/")[0];
+      const domain = domainWithNetwork.split("/")[1];
+
+      return `/wrapper/ens/${network}/${domain}`;
+    } else {
+      const network = "mainnet";
+      const domain = domainWithNetwork;
+
+      return `/wrapper/ens/${network}/${domain}`;
+    }
+  } else if (wrapUri.startsWith("ipfs/")) {
+    const cid = wrapUri.slice("ipfs/".length, wrapUri.length);
+
+    return `wrapper/ipfs/${cid}`;
+  }
+
+  return "";
+};
 
 const WrapperDependencyView: React.FC<{
   wrapperInfo: WrapperInfo;
 }> = ({ wrapperInfo }) => {
-  const { library: provider } = useEthers();
-
   return (
     <div className="WrapperDependencyView widget">
       <div className="">
         {wrapperInfo?.dependencies.map((wrapUri: string) => (
-          <div className="clickable p-2" key={wrapUri}>
-            <span
-              onClick={async () => {
-                if (!provider) {
-                  return;
-                }
-
-                if (wrapUri.startsWith("wrap://ens/")) {
-                  const domainWithNetwork = wrapUri.slice(
-                    "wrap://ens/".length,
-                    wrapUri.length
-                  );
-                  if (domainWithNetwork.includes("/")) {
-                    const network = domainWithNetwork.split("/")[0];
-                    const domain = domainWithNetwork.split("/")[1];
-                  } else {
-                    const network = "mainnet";
-                    const domain = domainWithNetwork;
-
-                    // TODO: go to wrapper url
-                    // setPublishedWrapper({
-                    //   ensDomain: {
-                    //     name: domain,
-                    //     chainId: 1,
-                    //   },
-                    // });
-                  }
-                } else if (wrapUri.startsWith("wrap://ipfs/")) {
-                  const cid = wrapUri.slice(
-                    "wrap://ipfs/".length,
-                    wrapUri.length
-                  );
-
-                  // TODO: go to wrapper url
-                  //   setPublishedWrapper({
-                  //   cid,
-                  // });
-                }
-              }}
-            >
+          <Link href={getDependencyUrl(wrapUri)} key={wrapUri}>
+            <span className="clickable p-2" key={wrapUri}>
               {wrapUri}
             </span>
-          </div>
+          </Link>
         ))}
         {wrapperInfo &&
           !(wrapperInfo.dependencies && wrapperInfo.dependencies.length) && (
